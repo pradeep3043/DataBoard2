1d89236aca19d1d23ba26299f16d6a22081fb601
Pradeep <Pradeep>
2016-05-03 10:08:08 +0200

    first

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour 
+{
+	public Transform board, building, building1Options;
+
+	public void Building (bool clicked){	
+		if (clicked == true) {
+			building.gameObject.SetActive (clicked);
+		} 
+
+		else {
+			building.gameObject.SetActive (clicked);
+		}
+	}
+
+	public void Building1Options (bool clicked) {
+		if (clicked == true) {
+			building.gameObject.SetActive (false);
+			building1Options.gameObject.SetActive (clicked);
+		}
+	}
+
+}
